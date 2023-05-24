4feb78f6da5377828c4d99744dddcdb094e62f1d
IvanSH-Jr <ivan_parabel@mail.ru>
2023-05-24 23:26:17 +0300

    add mock data and listener

--- script/main.js
+++ script/main.js
@@ -1,4 +1,22 @@
 import { dateInfo } from './dateParser.js'
+import {CATEGORY_1, CATEGORY_2, CATEGORY_3, CATEGORY_4} from './mockData.js'
 
-let date = dateInfo()
-console.log(date)
+
+
+CATEGORY_1.forEach(({id, img, cardName, date}) => {
+
+})
+
+const category = document.querySelectorAll('.nav__link')
+
+category.forEach(link => {
+    link.addEventListener('click', function(event){
+        let currentLink = link
+        let categoryId = currentLink.getAttribute('data-cat')
+        console.log(categoryId)
+        let currentCat = document.querySelector(categoryId)
+        console.log(currentCat)
+        category.forEach(link => {link.classList.remove('active')})
+        currentCat.classList.add('active')
+    })
+})
